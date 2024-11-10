4b301f4042b57ca1d37bba39b8c432d40eecbdf5
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-11-10 22:09:59 +0530

    FEAT: Linking Ops And Build target config

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,9 @@
 #![no_std] 
 #![no_main]
 
+#[allow(unused_imports)]
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start()-> !{ 
-    loop{}
-}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -15,3 +12,7 @@ fn panic(_info: &PanicInfo) -> !{
     loop{}
 }
 
+#[no_mangle]
+pub extern "C" fn _start()-> !{ 
+    loop{}
+}
