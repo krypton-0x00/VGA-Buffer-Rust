79b9c75b80f097c8dc846674b1d109127fe45e1f
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-11-11 12:30:45 +0530

    FEAT:print text using VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-
-#![no_std] 
+#![no_std]
 #![no_main]
 
 #[allow(unused_imports)]
 use core::panic::PanicInfo;
 
-
-#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
-    loop{}
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
 
+static HELLO: &[u8] = b"Hello World";
+
 #[no_mangle]
-pub extern "C" fn _start()-> !{ 
-    loop{}
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
